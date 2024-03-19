e1574e3818a7bc5236cdec5161d59dcfcfd267c9
https-404 <areeshali@gmail.com>
2024-03-20 00:16:43 +0500

    Added login function and update register function in userController module

--- userController/usercontroller.js
+++ userController/usercontroller.js
@@ -1,12 +1,70 @@
+/**
+ * User Controller module.
+ * @module userController
+ */
+
 const UserModel = require('../models/userModel');
+const jwt = require('jsonwebtoken');
 const bcrypt = require('bcrypt');
 
 module.exports = {
-    // Create and Save a new User
-     login:(req, res) => {
-        res.send('Login Success');
+    /**
+     * Login function to authenticate user.
+     * @async
+     * @function login
+     * @param {Object} req - The request object.
+     * @param {Object} res - The response object.
+     * @returns {Object} - The response object containing JWT token and user object.
+     */
+    login: async (req, res) => {
+        try {
+            const user = await UserModel.findOne({email: req.body.email});
+            if (!user) {
+                return res.status(404).json(
+                    {
+                        message: 'Auth Failure!, User Not Found. Please Register'
+                    }
+                )
+            }
+
+            const validPassword = await bcrypt.compare(req.body.password, user.password);
+            if (!validPassword) {
+                return res.status(401).json(
+                    {
+                        message: 'Auth Failure!, Invalid Password OR User'
+                    }
+                )
+            }
+
+            const tokenObject = {
+                email: user.email,
+                userId: user._id,
+                fullname: user.fullname,
+                role : user.role
+            }
+
+            const jwttoken = jwt.sign(tokenObject, process.env.SECRET, {expiresIn: '1h'});
+
+            return res.status(200).json(
+                {
+                    jwttoken, tokenObject
+                }
+            )
+
+            
+        } catch (error) {
+            return res.status(500).json({message: 'error', error});
+        }
+       
     },
-    //register user
+    /**
+     * Register function to create and save a new user.
+     * @async
+     * @function register
+     * @param {Object} req - The request object.
+     * @param {Object} res - The response object.
+     * @returns {Object} - The response object containing success message and user data.
+     */
     register: async (req, res) => {
         const userModel = new UserModel(req.body);
         userModel.password = await bcrypt.hash(req.body.password, 10);
