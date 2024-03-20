58b3ec80fa5a9d9325a2de41a696747ab6295c59
https-404 <areeshali@gmail.com>
2024-03-20 23:37:23 +0500

    Refactor routes and add getUsers endpoint jwt configured middlewares working
    last commit until i get an idea what to do next

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,51 @@
-// /**
-//  * Express router for handling routes related to user registration and login.
-//  * @module routes
-//  */
+/**
+ * Express router for handling routes related to user registration and login.
+ * @module routes
+ */
 
 const express = require('express');
-const { register, login } = require('../userController/usercontroller');
+const { register, login, getUsers } = require('../userController/usercontroller');
 const { userRegisterValidation, userLoginValidation } = require('../Middleware/UserValidation');
+const { tokenAuthentication } = require('../utils/auth');
 
 const routes =  express.Router();
 
-// /**
-//  * Route handler for the root endpoint.
-//  * @name GET /
-//  * @function
-//  * @param {Object} req - Express request object.
-//  * @param {Object} res - Express response object.
-//  */
+/**
+ * Route handler for the root endpoint.
+ * @name GET /
+ * @function
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 routes.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-// /**
-//  * Route handler for user registration.
-//  * @name POST /register
-//  * @function
-//  * @param {Object} req - Express request object.
-//  * @param {Object} res - Express response object.
-//  */
-
+/**
+ * Route handler for user registration.
+ * @name POST /register
+ * @function
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 routes.post('/register',userRegisterValidation, register);
 
-// /**
-//  * Route handler for user login.
-//  * @name POST /login
-//  * @function
-//  * @param {Object} req - Express request object.
-//  * @param {Object} res - Express response object.
-//  */
+/**
+ * Route handler for user login.
+ * @name POST /login
+ * @function
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
 routes.post('/login',userLoginValidation, login);
+
+/**
+ * Route handler for getting all users.
+ * @name GET /users
+ * @function
+ * @param {Object} req - Express request object.
+ * @param {Object} res - Express response object.
+ */
+routes.get('/users',tokenAuthentication, getUsers);
 
 module.exports = routes;
